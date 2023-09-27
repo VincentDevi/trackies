ca63a5eebc9ad7bd95fd8ad88d27aa18465a662a
Devilers Vincent <vincent.devilers@gmail.com>
2023-09-27 16:19:03 +0200

    add a toast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,13 @@ import { type AppType } from "next/app";
 import "@/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { api } from "@/utils/api";
+import { Toaster } from "@/ui-lib/components/toaster";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
       <Component {...pageProps} />
+      <Toaster />
     </ClerkProvider>
   );
 };
